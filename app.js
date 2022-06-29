aeb31bcc12e85d552597c6a31f31922267758726
Julia Schmaglowski <jschmaglowski@aol.com>
2022-06-29 11:26:46 +0200

    refactor sockets

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 const express = require("express");
-var cors = require("cors");
+const cors = require("cors");
 const InMemorySessionStore = require("./session_store.js");
+const http = require('http');
+const { Server } = require("socket.io");
+const WebSocket = require('ws');
+const { v4: uuidv4 } = require('uuid');
+
 const app = express();
 app.use(express.json());
-const port = 4000;
-const http = require('http');
+
+/*
+     FRONTEND
+*/
+
+let sessionStore = new InMemorySessionStore();
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-//const td = require("socket.io-client");
-const io = new Server(server, {
+const frontendIO = new Server(server, {
   cors: {
     origin: "http://localhost:3000"
   }
 });
-
-const WebSocket = require('ws')
-const ws = new WebSocket("ws://192.168.2.124:6001"); //touch desiner wlan ip + port
-
-//const socketTd = td("ws://127.0.0.1:6000");
-
-const { v4: uuidv4 } = require('uuid');
-
-let sessionStore = new InMemorySessionStore();
 
 var corsOptions = {
   origin: '*',
@@ -29,30 +27,7 @@ var corsOptions = {
 }
 app.use(cors(corsOptions))
 
-let queue = [];
-
-//digest für SocketIO
-/*const digest = (socketTd) => {
-  item = queue.shift();
-  setTimeout(() => digest(socketTd), 15000);
-  if (item) {
-    socketTd.emit("play_sound" , item.soundName);
-    console.log(`digest: ${item.soundName} from ${item.id}`);
-  }
-};
-digest(socketTd);*/
-
-const digest = (WebSocket) => {
-  item = queue.shift();
-  setTimeout(() => digest(WebSocket), 15000);
-  if (item) {
-    console.log(`digest: ${item.soundName} from ${item.id}`);
-    ws.send(item.soundName);
-  }
-};
-digest(WebSocket);
-
-io.use((socket, next) => {
+frontendIO.use((socket, next) => {
   const sessionID = socket.handshake.auth.sessionID;
   if(sessionID) {
     //find existing session
@@ -72,9 +47,8 @@ io.use((socket, next) => {
   next();
 });
 
-
 //send session details to user
-io.on('connection', (socket) => {
+frontendIO.on('connection', (socket) => {
   console.log('a user connected');
   sessionStore.saveSession(socket.sessionID, {
     userID: socket.userID,
@@ -87,66 +61,50 @@ io.on('connection', (socket) => {
   });
 
   socket.on("add_to_queue", (id, soundName) => {
+    position = queue.findIndex((item) => item.id == id);
+    if (position < 0){
     queue.push({ id: id, soundName: soundName });
     console.log(id,soundName);
     socket.emit("position", queue.length);
+    }
   })
 });
 
-/*app.get("/api/queue/check", (req, res) => {
-  if (req.query && req.query.id) {
-    position = queue.findIndex((item) => item.id == req.query.id);
-    if (position < 0) {
-      res.status(404).json({
-        success: true,
-        status: 404,
-        position: -1,
-        message: "user has no item in queue",
-      });
-    } else {
-      res.json({
-        success: true,
-        status: 200,
-        position: position + 1
-      });
-    }
-  } else {
-    res.status(400).json({
-      success: false,
-      status: 400,
-      message: "id is missing",
-    });
-  }
-});
+/*
+     TOUCH DESIGNER
+*/
 
-app.post("/api/add-to-queue", (req, res) => {
-  if (req.body && req.body.id && req.body.soundName) {
-    position = queue.findIndex((item) => item.id == req.body.id);
-    if (position < 0) {
-      queue.push({ id: req.body.id, soundName: req.body.soundName });
-      res.status(201).json({
-        success: true,
-        status: 201,
-        position: queue.length,
-        message: "item enqueued",
-      });
-    } else {
-      res.json({
-        success: false,
-        status: 200,
-        position: position,
-        message: "user already has something in da queue",
-      });
-    }
-  } else {
-    res.status(400).json({
-      success: false,
-      status: 400,
-      message: "id or soundName is missing",
-    });
-  }
-});
+const touchDesignerServer = new WebSocket.Server({port:5050});
 
+let socket = null;
+
+touchDesignerServer.on('connection', (ws) => {
+  var initMessage = {message:"connection"};
+  ws.send(JSON.stringify(initMessage));
+  console.log('new client');
+  socket = ws;
+})
+
+/*
+     DIGEST
+*/
+
+let queue = [
+];
+
+const digest = () => {
+  item = queue.shift();
+  setTimeout(() => digest(), 13000);
+  if (item && socket && frontendIO) {
+    console.log(`digest: ${item.soundName} from ${item.id}`);
+    socket.send(item.soundName);
+    frontendIO.emit("positionUpdate");
+  }
+};
+digest();
+
+
+/*
 app.delete("/api/queue/clear", (req, res) => {
   queue = [];
   res.status(204).send();
@@ -180,6 +138,9 @@ app.post("/api/queue/add-priority", (req, res) => {
   }
 });
 */
+
+const port = 4000;
+
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
